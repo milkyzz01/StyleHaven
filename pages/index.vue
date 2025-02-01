5873c77c5b11370183c697f96baa4fea2f3f4342
Menard <menardluiscaponpon@gmail.com>
2025-02-01 15:45:03 +0800

    feat: added initial view for collection section in landing page

--- pages/index.vue
+++ pages/index.vue
@@ -36,6 +36,17 @@
         Step into a world where elegance meets innovation, and let your style tell your story.
     </p>
     </div>
+    <!-- Collection types -->
+    <div class="mt-10 relative grid grid-cols-1 lg:grid-cols-3 w-full gap-5 px-10 py-40 md:p-40">
+        <img src="../assets/formal.webp" class="w-[100%] h-[450px]" alt="formal">
+        <img src="../assets/casual.webp" class="w-[100%] h-[450px]" alt="casual">
+        <img src="../assets/streetwear.webp" class="w-[100%] h-[450px]" alt="street">
+        <div class="custom-shape-divider-bottom-1737879043">
+            <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
+                <path d="M985.66,92.83C906.67,72,823.78,31,743.84,14.19c-82.26-17.34-168.06-16.33-250.45.39-57.84,11.73-114,31.07-172,41.86A600.21,600.21,0,0,1,0,27.35V120H1200V95.8C1132.19,118.92,1055.71,111.31,985.66,92.83Z" class="shape-fill bg-red-700"></path>
+            </svg>
+        </div>
+    </div>
 </template>
 
 <script setup lang="ts">
